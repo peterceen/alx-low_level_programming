91711e65f4acd33f978317d2b64ecb6291ecf216
peterceen <104788042+peterceen@users.noreply.github.com>
2022-06-06 16:10:54 -0700

    5-printf.c0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Description: this project helps to solve my c challange
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
