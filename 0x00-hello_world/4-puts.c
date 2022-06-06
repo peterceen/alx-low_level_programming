c130110bf706cb1d6e7f7c027f67051bdf1f2736
peterceen <104788042+peterceen@users.noreply.github.com>
2022-06-06 15:34:50 -0700

    4-puts.c0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Returns: always 0(Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
