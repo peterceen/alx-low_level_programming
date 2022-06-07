6c49276709531abef8b5688f6b52c71569e3035a
peterceen <104788042+peterceen@users.noreply.github.com>
2022-06-06 17:47:49 -0700

    101-quote itteration 0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,19 +6,17 @@
 
 /**
  *
- *  * main - Entry point
+ *  main - Entry point
  *
- *   * Return: Always 1 (Success)
+ *  Return: Always 1 (Success)
  *
- *    */
+ */
 
 int main(void)
-
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
+		write(STDOUT_FILENO, "and that piece of art is useful
 
 							\" - Dora Korpar, 2015-10-19\n", 59);
-
 			return (1);
 
 }
