ebe0ce125a5636824c08b357d59a46a102afe6f8
peterceen <104788042+peterceen@users.noreply.github.com>
2022-06-16 20:40:39 -0700

    1-strncat.c itteration

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *  _strncat -a function that concatenates two strings
+ *  @dest: first input string
+ *  @src: second input string
+ *  @n: integer input
+ *  Returm: pointer to the final string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	while (*dest)
+		dest++;
+
+	if(n > srclen)
+		n=srclen;
+
+	src = start;
+
+	for(; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
