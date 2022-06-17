716a91ef4b9f00c1379a24c879b3621770af1e20
peterceen <104788042+peterceen@users.noreply.github.com>
2022-06-16 19:32:02 -0700

    0-strcat.c itteration

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <studio.h>
+
+/**
+ * _strcat -a function that concatenates strings
+ * @dest: first input string
+ * @src: second input string
+ * Return: pointer to string
+ */
+char *_strcat(char *dest, char*src)
+{	
+	char *temp = dest;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
