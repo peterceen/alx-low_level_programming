b6c7145eeb3d4fef960ed1c16a3aac4502c13ccc
peterceen <104788042+peterceen@users.noreply.github.com>
2022-06-17 20:42:59 -0700

    3-print_alphabets.c itteration

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,14 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
 	putchar('\n');
 
