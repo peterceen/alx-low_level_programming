d8de394a93c811c95aac00f32fa643711510a632
peterceen <104788042+peterceen@users.noreply.github.com>
2022-09-08 11:55:26 -0700

    my life is getting this right

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@
  *
  */
 int main () {
-	write(STDOUT_FILENO, "and that piece of art is useful
+		fwrite(STDOUT_FILENO, "and that piece of art is useful
 				\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
