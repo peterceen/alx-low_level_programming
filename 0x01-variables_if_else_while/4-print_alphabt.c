fda5bc842fdae0bca24a220c0f34d52446feacf0
peterceen <104788042+peterceen@users.noreply.github.com>
2022-09-09 12:06:45 -0700

    print letters skipping two letters e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>                                                                                                                                     
+#include <stdlib.h>                                                                                                                                    
+#include <time.h>                                                                                                                                                                                                                                                                                            
+
+/*
+ * main - Entry point                                                                                                                                  
+ *
+ *Return: Always 0 (Success)                                                                                                                     
+ *
+*/                                                                                                                                                    
+int main(void)                                                                                                                                         
+{                                                                                                                                                      
+	char low;
+	for (low ='a'; low <= 'z';low++){
+		switch(low) {
+			case 'e':
+			low = low + 1;
+			break;
+			case 'q':
+			low =low + 1;
+		break;
+		}
+
+		putchar(low);
+	}
+	     putchar('\n');
+	return (0);                                                                                                                           
+}
