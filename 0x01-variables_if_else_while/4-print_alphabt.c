0e105b00b64b9b084affd8207c3a6d2014d9b82b
peterceen <104788042+peterceen@users.noreply.github.com>
2022-09-09 12:36:59 -0700

    This is a better naming of the file for alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,28 +1,30 @@
-#include <stdio.h>                                                                                                                                     
-#include <stdlib.h>                                                                                                                                    
-#include <time.h>                                                                                                                                                                                                                                                                                            
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /*
- * main - Entry point                                                                                                                                  
+ * main: Entry point
+ *Return: Always 0 (Success)
  *
- *Return: Always 0 (Success)                                                                                                                     
- *
-*/                                                                                                                                                    
-int main(void)                                                                                                                                         
-{                                                                                                                                                      
-	char low;
-	for (low ='a'; low <= 'z';low++){
-		switch(low) {
-			case 'e':
+*/
+
+int main(void)
+
+{  char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		switch (low)
+		{
+		case 'e':
 			low = low + 1;
-			break;
-			case 'q':
-			low =low + 1;
+		break;
+		case 'q':
+			low = low + 1;
 		break;
 		}
-
 		putchar(low);
 	}
-	     putchar('\n');
-	return (0);                                                                                                                           
+	putchar('\n');
+	return (0);
 }
