9b2fe3bec90fe63bf837b097095df8c2c4443328
peterceen <104788042+peterceen@users.noreply.github.com>
2022-10-05 17:42:40 -0700

    it is printers intergs

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+ #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main- a programe to print two int
+ *
+ * Description it does not repeat intergers and is seperated by comma
+ *
+ * Return Always(0)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	for(a='0'; a<='9'; a++)
+	{
+		for(b=a+1; b<='9'; b++)
+		{
+			if (a!=b)
+			{	
+				putchar(a);
+				putchar(b);
+			 if(a=='8'&&b=='9')
+				 continue;
+			putchar(',');
+			putchar(' ');
+		
+			
+			}
+		}
+	} putchar('\n');
+	return (0);
+}
