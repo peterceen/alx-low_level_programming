9555ae826f474b7c461561eaa7f56f1982f9a82a
peterceen <104788042+peterceen@users.noreply.github.com>
2022-10-08 09:35:46 -0700

    a good list od non repeating numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -14,20 +14,18 @@ int main(void)
 {
 	int a;
 	int b;
-	for(a='0'; a<='9'; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for(b=a+1; b<='9'; b++)
+		for (b = a + 1; b <= '9'; b++)
 		{
-			if (a!=b)
-			{	
+			if (a != b)
+			{
 				putchar(a);
 				putchar(b);
-			 if(a=='8'&&b=='9')
-				 continue;
+			if (a == '8' && b == '9')
+				continue;
 			putchar(',');
 			putchar(' ');
-		
-			
 			}
 		}
 	} putchar('\n');
