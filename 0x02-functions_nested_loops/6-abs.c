0a630a5ad59ea41963a166a2ae0463f60f21c6a3
peterceen <104788042+peterceen@users.noreply.github.com>
2022-10-18 05:13:30 -0700

    computes absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs -the modulo
+ *
+ * description: this converts all values to absolute values
+ *
+ * return: Always 0.
+ */
+
+int _abs(int c)
+{
+	int a = -1;
+	if (c < 0)
+	{
+		c = c/a;
+	}
+	return (c);
+}
