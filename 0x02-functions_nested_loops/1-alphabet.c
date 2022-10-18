1789a931c79c958e1705ad3c199ab58cda39c6f1
peterceen <104788042+peterceen@users.noreply.github.com>
2022-10-18 00:40:20 -0700

    prints characters of the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
-/*
- * print_alphabet -check description
+/**
+ * main-entry point
  *
+ * description: this code prints alphabet
  *
- * Return: nothing
+ * return Always(0)
  */
 
-void  print_alphabet(void)
+void print_alphabet(void)
 {
-
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		putchar(i);
-		putchar('\n');
-
+	for(i = 'a'; i < 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -the entry point
+ *
+ * description: the reference to main code
+ *
+ * return Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
