8e19800a5b6f0eed1e04e6863b4593a5f0cfbe23
peterceen <104788042+peterceen@users.noreply.github.com>
2022-10-18 01:15:19 -0700

    prints characters of the alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  print_alphabet_x10 -prints characters 10 times
+ *  description: this print a through z ten times
+ * 
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
