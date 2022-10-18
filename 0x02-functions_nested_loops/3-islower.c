22bd6b302f819b1c25a86bbd007dff5ac0540c70
peterceen <104788042+peterceen@users.noreply.github.com>
2022-10-18 02:26:59 -0700

    checks lower or higher letters  of the alphabet

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,10 @@
 
 /**
  * _islower - base code
- * @c: integer variable 
+ * @c: integer variable
  * description: checks if input is uppercase or lower case
  *
- * return: Success 0  1
+ * Return: 1 if is lowercase or 0 if is uppercase
  */
 
 int _islower(int c)
