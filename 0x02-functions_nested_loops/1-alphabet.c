03efdd87aa90cc122f06370e76d3275137d5082a
peterceen <104788042+peterceen@users.noreply.github.com>
2022-10-18 00:47:10 -0700

    prints characters of the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main-entry point
+ * main -entry point of code print_alphabet
  *
- * description: this code prints alphabet
+ * Description : this code prints_alphabet
  *
  * return Always(0)
  */
@@ -12,7 +12,7 @@ void print_alphabet(void)
 {
 	char i;
 
-	for(i = 'a'; i < 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
 }
