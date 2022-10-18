fe7afbbbe2f39ba38c37004984621d37e611443c
peterceen <104788042+peterceen@users.noreply.github.com>
2022-10-18 01:19:09 -0700

    prints characters of the alphabet ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,6 @@
 /**
  *  print_alphabet_x10 -prints characters 10 times
  *  description: this print a through z ten times
- * 
  */
 
 void print_alphabet_x10(void)
@@ -14,7 +13,7 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
-			_putchar(i);
+			_putchar(j);
 		_putchar('\n');
 	}
 }
