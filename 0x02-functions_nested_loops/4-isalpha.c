99f6d8ab1dcbe2edf61548411e7e46a76793b233
peterceen <104788042+peterceen@users.noreply.github.com>
2022-10-18 02:50:25 -0700

    checks if input is an alphabet or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - base code
+ * @c: integer variable
+ * description: checks if input is uppercase or lower case
+ *
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+int _isalpha(int c)
+{
+	if  (('a' <= c && c <= 'z')|| ('A' <= c && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	return (0);
+}
