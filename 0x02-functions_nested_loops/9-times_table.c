cd47be36cdf63e8fde9330041f602c456e3db8e8
peterceen <104788042+peterceen@users.noreply.github.com>
2022-10-21 22:33:18 -0700

    prints 9 times table in table format

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,6 +11,7 @@
 void times_table(void)
 {
 	int i, j, n;
+	
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
@@ -21,14 +22,13 @@ void times_table(void)
 				if (j != 0)
 					_putchar(' ');
 				_putchar(n + '0');
-				
 				if (j == 9)
 					continue;
 				_putchar(',');
 				_putchar(' ');
 			}
 			else
-			{	
+			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
 				if (j == 9)
