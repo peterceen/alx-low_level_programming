a28bee12f694e35a369b83032ed126213e35129e
peterceen <104788042+peterceen@users.noreply.github.com>
2022-10-22 11:53:55 -0700

    prints timetable of integer n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,29 +16,47 @@ void print_times_table(int n)
 	if (n > 0 && n < 15)
 	for (i = 0; i <= n; i++)
 	{
+		_putchar('0');
+
 		for (j = 0; j <= n; j++)
 		{
 			a = i * j;
-			if ((a / 10) == 0)
-			{
-				if (j != 0)
-					_putchar(' ');
-				_putchar(a + '0');
-				if (j == n)
-					continue;
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((a / 10) + '0');
-				_putchar((a % 10) + '0');
-				if (j == n)
-					continue;
-				_putchar(',');
-				_putchar(' ');
-			}
+			tableformat(a);
 		}
-		_putchar('\n');
+	}
+}
+
+
+/**
+ * tableformat - formatted characters to output
+ * @a: number to format
+ * Return: nothing
+ */
+
+void tableformat(int a)
+{
+	if (a <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(a + '0');
+	}
+	else if (a > 9 && a <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(a / 10 + '0');
+		_putchar(a % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(a / 100 + '0');
+		_putchar(a / 10 % 10 + '0');
+		_putchar(a % 10 + '0');
 	}
 }
