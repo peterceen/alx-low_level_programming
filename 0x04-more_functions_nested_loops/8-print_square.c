91c3d8c5677b5a8d1bf93edc2f7fcfdf97dc7f1a
peterceen <104788042+peterceen@users.noreply.github.com>
2022-11-15 08:58:47 -0800

    squares by sizes.

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,13 +15,12 @@ void print_square(int size)
 
 		for (a = 0; a <= size; a++)
 		{
-		for (b = 0; b <= size; b++)
-		{
-			_putchar(35);
+			for (b = 0; b <= size; b++)
+			{
+				_putchar(35);
+			_putchar('\n');
+			}
 		}
-		_putchar('\n');
-		}
-	}
 	else
 		_putchar('\n');
 
