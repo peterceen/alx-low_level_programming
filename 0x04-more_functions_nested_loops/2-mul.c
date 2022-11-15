70f95aa870a00fe7b39f381cdf5b0eb816deaadd
peterceen <104788042+peterceen@users.noreply.github.com>
2022-11-15 05:24:06 -0800

    check product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * mul- A function that multiplies two numbers
+ *
+ * @a input variable 1
+ * @b input variable 2
+ *
+ * Description: the function descroiption
+ *
+ * return: the value of the function.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
