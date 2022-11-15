64d6930f3cec2511930152bf4c54f5b00fae3499
peterceen <104788042+peterceen@users.noreply.github.com>
2022-11-15 08:43:55 -0800

    squares by sizes.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * print_square -Afunction that prints square.
+ * @size parameter for size
+ * return: a value of 0.
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 0; a <= size; a++)
+	{
+		for (b = 0; b <= size; b++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+
+}
